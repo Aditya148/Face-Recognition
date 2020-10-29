fe15b727a1dad39745652d43357962ab626c4889
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-10-29 15:22:53 +0530

    Add files via upload
    Program Files Added

---
+++ face-recog.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 14:49:19 2019
+@author: krish.naik
+"""
+
+# Face Recognition
+
+# Importing the libraries
+from PIL import Image
+from keras.applications.vgg16 import preprocess_input
+import base64
+from io import BytesIO
+import json
+import random
+import cv2
+from keras.models import load_model
+import numpy as np
+
+from keras.preprocessing import image
+
+#Load the trained model 
+model = load_model('Models/facefeatures_new_model.h5')
+
+# Loading the cascades
+#Path to Haarcascade  classifier
+face_cascade = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
+
+def face_extractor(img):
+    # Function detects faces and returns the cropped face
+    # If no face detected, it returns the input image
+    
+    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+
+# Doing some Face Recognition with the webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    #canvas = detect(gray, frame)
+    #image, face =face_detector(frame)
+    
+    face=face_extractor(frame)
+    if type(face) is np.ndarray:
+        face = cv2.resize(face, (224, 224))
+        im = Image.fromarray(face, 'RGB')
+           #Resizing into 128x128 because we trained the model with this image size.
+        img_array = np.array(im)
+                    #Our keras model used a 4D tensor, (images x height x width x channel)
+                    #So changing dimension 128x128x3 into 1x128x128x3 
+        img_array = np.expand_dims(img_array, axis=0)
+        pred = model.predict(img_array)
+        print(pred)
+                     
+        name="None matching"
+        
+        if(pred[0][0]>0.5):
+            name='Aditya'
+        cv2.putText(frame,name, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+    else:
+        cv2.putText(frame,"No face found", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ train-test-set-generator.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier('Face-Recognition/haarcascade-frontalface-default.xml')
+
+def face_extractor(img):
+    # Function detects faces and returns the cropped face
+    # If no face is detected, it returns the input image
+    faces = face_classifier.detectMultiScale(img, 1.3, 5)
+    
+    if faces is ():
+        return None
+    
+    # Crop all faces found
+    for (x,y,w,h) in faces:
+        x=x-10
+        y=y-10
+        cropped_face = img[y:y+h+50, x:x+w+50]
+
+    return cropped_face
+
+# Initialize Webcam
+cap = cv2.VideoCapture(0)
+count = 0
+
+while True:
+
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        face = cv2.resize(face_extractor(frame), (400, 400))
+
+        # Save file in specified directory with unique name
+        file_name_path = '/Datasets/Train/person/' + str(count) + '.jpg'
+        cv2.imwrite(file_name_path, face)
+
+        # Put count on images and display live count
+        cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.imshow('Face Cropper', face)
+        
+    else:
+        print("Face not found")
+        pass
+
+    if cv2.waitKey(1) == 13 or count == 100: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()      
+print("Collecting Samples Complete")
